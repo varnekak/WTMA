a2fbc53573b908eca7e39fb8dc438ccea6e72062
Varneka K <varnekak2005@gmail.com>
2026-03-13 15:57:26 +0530

    Add files via upload

---
+++ exp4/src/main/java/UserServlet.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		
+		response.getWriter().println("<html><body>");
+		response.getWriter().println("<h2> Hello, " +name+ "! You are " +age+ " years old.</h2>");
+		response.getWriter().println("</body></html>");
+		
+	}
+
+}
